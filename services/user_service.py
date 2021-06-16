43c2f3e41c6dfa5ee9897591321d49e132bbe820
Yasser Tahiri <yasserth19@gmail.com>
2021-06-16 01:38:33 +0100

    Create the services folder with a auth configuration :rocket:

---
+++ services/user_service.py
@@ -0,0 +1,10 @@
+from auth import auth_handler
+import config
+
+
+def user_login(name):
+
+    access_token = auth_handler.create_access_token(
+        user_id=name, expiretime=config.auth_expiretime)
+
+    return {'id': 1, 'name': name, 'token': access_token}
