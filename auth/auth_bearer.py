5cdbf3192c5f5b3e0088cf591630c2e7b3f987e0
Yasser Tahiri <yasserth19@gmail.com>
2021-07-15 05:15:09 +0100

    💎 Fix The JWT Secret :rocket:

--- auth/auth_bearer.py
+++ auth/auth_bearer.py
@@ -1,4 +1,3 @@
-
 from fastapi import Request, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
@@ -10,18 +9,19 @@ class JWTBearer(HTTPBearer):
         super(JWTBearer, self).__init__(auto_error=auto_error)
 
     async def __call__(self, request: Request):
-        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        credentials: HTTPAuthorizationCredentials = await super(
+            JWTBearer, self).__call__(request)
         if credentials:
             if not credentials.scheme == "Bearer":
-                raise HTTPException(
-                    status_code=403, detail="Invalid authentication scheme.")
+                raise HTTPException(status_code=403,
+                                    detail="Invalid authentication scheme.")
             if not self.verify_jwt(credentials.credentials):
-                raise HTTPException(
-                    status_code=403, detail="Invalid token or expired token.")
+                raise HTTPException(status_code=403,
+                                    detail="Invalid token or expired token.")
             return credentials.credentials
         else:
-            raise HTTPException(
-                status_code=403, detail="Invalid authorization code.")
+            raise HTTPException(status_code=403,
+                                detail="Invalid authorization code.")
 
     def verify_jwt(self, jwtoken: str) -> bool:
         isTokenValid: bool = False
--- auth/auth_handler.py
+++ auth/auth_handler.py
@@ -1,17 +1,17 @@
 import time
 import jwt
+import os
 
 from typing import Dict
 
-JWT_SECRET = "jeffQL"
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+JWT_SECRET = str(os.environ.get("JWT_SECRET"))
 JWT_ALGORITHM = "HS256"
 
 
 def create_access_token(user_id: str, expiretime: int) -> Dict[str, str]:
-    payload = {
-        "user_id": user_id,
-        "expires": time.time() + expiretime
-    }
+    payload = {"user_id": user_id, "expires": time.time() + expiretime}
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
     return token.decode('ascii')
@@ -19,8 +19,10 @@ def create_access_token(user_id: str, expiretime: int) -> Dict[str, str]:
 
 def decode_access_token(token: str) -> dict:
     try:
-        decoded_token = jwt.decode(
-            token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-        return decoded_token if decoded_token["expires"] >= time.time() else None
+        decoded_token = jwt.decode(token,
+                                   JWT_SECRET,
+                                   algorithms=[JWT_ALGORITHM])
+        return decoded_token if decoded_token["expires"] >= time.time(
+        ) else None
     except:
         return {}
